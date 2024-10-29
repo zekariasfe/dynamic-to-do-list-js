2af62164d5b3a6cf85c200f122d701f2193e724d
zekariasfe <zekariasfekeru@gmail.com>
2024-10-29 21:05:38 +0300

    to do list app

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function addTask() {
 
         const removeButton = document.createElement('button');
         removeButton.textContent = 'Remove';
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn');
         removeButton.onclick= function() {
             taskList.removeChild(li);
         };
